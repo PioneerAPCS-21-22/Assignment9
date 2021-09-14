ad801db9d39d8fe1081f67e13ce5c043912b853f
codytheking <codytheking93@gmail.com>
2021-09-13 22:27:39 -0700

    Starter code

---
+++ Main.java
@@ -0,0 +1,59 @@
+/**
+ * This program does a few number operations.
+ *
+ * @author
+ * @version
+ */
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        // prompt and take input for name
+
+
+        // greet user by name and print instructions
+
+
+        // call method and print sum of digits
+
+
+        // call method and print reversed digits
+
+    }
+
+    /**
+     * Prints a greeting and instructions.
+     *
+     * @param name the name of the user
+     */
+    // method heading
+    {
+        // greet user
+
+        // prompt user to input a four-digit number
+    }
+
+    /**
+     * Calculates the sum of the digits.
+     *
+     * @param num the number to analyze
+     * @return sum of the digits in num as an int
+     */
+    // method heading
+    {
+        // your implementation
+    }
+
+    /**
+     * Reverses the digits.
+     *
+     * @param num the number to analyze
+     * @return the num with the digits reversed as an int
+     */
+    // method heading
+    {
+        // your implementation
+    }
+}
+
